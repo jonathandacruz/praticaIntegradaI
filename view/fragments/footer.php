c659b00d4e2e01582f7c5bdb6bbbb63043947c19
Jonathan da Cruz <jonathanadcz@gmail.com>
2022-12-02 15:30:06 -0300

    fix: Footer Ajust Text

--- view/fragments/footer.php
+++ view/fragments/footer.php
@@ -4,36 +4,25 @@
         <div class="row">
             <div class="col-lg-3 col-md-6">
                 <div class="footer-box about-widget">
-                    <h2 class="widget-title">About us</h2>
-                    <p>Ut enim ad minim veniam perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
+                    <h2 class="widget-title">Sobre nós</h2>
+                    <p>A fruteira do parque foi fundada em 2008, no segmento de hortifruti. Estamos há mais de 10 anos, proporcionando praticidade atrelado a um bom atendimento.</p>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6">
                 <div class="footer-box get-in-touch">
-                    <h2 class="widget-title">Get in Touch</h2>
+                    <h2 class="widget-title">Onde encontrar?</h2>
                     <ul>
-                        <li>34/8, East Hukupara, Gifirtok, Sadan.</li>
-                        <li>support@fruitkha.com</li>
-                        <li>+00 111 222 3333</li>
+                        <li>Chapecó</li>
+                        <li>contato@fruitkha.com</li>
+                        <li>049 3546-8899</li>
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="footer-box pages">
-                    <h2 class="widget-title">Pages</h2>
-                    <ul>
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="services.html">Shop</a></li>
-                        <li><a href="news.html">News</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
+             
             <div class="col-lg-3 col-md-6">
                 <div class="footer-box subscribe">
-                    <h2 class="widget-title">Subscribe</h2>
-                    <p>Subscribe to our mailing list to get the latest updates.</p>
+                    <h2 class="widget-title">Confira as novidades</h2>
+                    <p>Cadastre seu melhor e-mail para receber nossa lista de novidades!</p>
                     <form action="index.html">
                         <input type="email" placeholder="Email">
                         <button type="submit"><i class="fas fa-paper-plane"></i></button>
